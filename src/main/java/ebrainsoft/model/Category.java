4b701f57305b3cf8c6de422c056deb9ee4a4043e
HyunggiPark <phg9587@naver.com>
2023-05-30 16:25:58 +0900

    feat : mybatis 로 category 조회 구현
    - application.properties 내용도 업데이트가 필요함 주의!

--- src/main/java/ebrainsoft/model/Category.java
+++ src/main/java/ebrainsoft/model/Category.java
@@ -1,8 +1,12 @@
 package ebrainsoft.model;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@Setter
+@ToString
 public class Category {
     Long categoryId;
     String name;
